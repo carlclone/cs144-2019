cdcd60362e8ddd4ec4b66efd4a83984d5ea76efd
lincongcong <lincongcong@tangbull.com>
2020-05-18 16:30:21 +0800

    update

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -22,23 +22,25 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
      *  如果不吻合,放到一个未重组的list中,或者map中 , 等待在它前面的index到达后 , 再取出来放入bytestream
      *  map的大小就是capacity , 最多只能有capacity个字节未重组
      * */
-    if (index == nextIndex) {
-        //注意min size
-        _output.write(data);
-    } else {
-        //更新unassembled bytes
-        //放到map中等待
-
-        //检查map中是否有match的,如果有,push_substring
-        if ("match") {
-            push_substring('string',nextIndex,false);
-        }
-    }
+//    if (index == nextIndex) {
+//        //注意min size
+//        _output.write(data);
+//    } else {
+//        //更新unassembled bytes
+//        //放到map中等待
+//
+//        //检查map中是否有match的,如果有,push_substring
+//        if ("match") {
+////            push_substring('string',nextIndex,false);
+//        }
+//    }
 }
 
 size_t StreamReassembler::unassembled_bytes() const {
 
-    return unassembytes;
+//    return unassembytes;
 }
 
-bool StreamReassembler::empty() const { return un ==0; }
+bool StreamReassembler::empty() const {
+//    return un ==0;
+}
