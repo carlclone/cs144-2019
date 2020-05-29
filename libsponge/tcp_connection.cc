a53cfbcde4eadb7c041ff4a8dab19527c8913384
lincongcong <lincongcong@tangbull.com>
2020-05-29 08:31:45 +0800

    helpers

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -12,17 +12,30 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const {
+    return _cfg.send_capacity - _sender.bytes_in_flight();
+}
 
-size_t TCPConnection::bytes_in_flight() const { return {}; }
+size_t TCPConnection::bytes_in_flight() const {
+    return _sender.bytes_in_flight();
+}
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const {
+    _receiver.unassembled_bytes();
+}
 
-size_t TCPConnection::time_since_last_segment_received() const { return {}; }
+size_t TCPConnection::time_since_last_segment_received() const {
+    return timeSinceLastSegmentReceived;
+}
 
-void TCPConnection::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
+void TCPConnection::segment_received(const TCPSegment &seg) {
+    timeSinceLastSegmentReceived=0;
+    DUMMY_CODE(seg);
+}
 
-bool TCPConnection::active() const { return {}; }
+bool TCPConnection::active() const {
+    return {};
+}
 
 size_t TCPConnection::write(const string &data) {
     DUMMY_CODE(data);
@@ -30,11 +43,17 @@ size_t TCPConnection::write(const string &data) {
 }
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
-void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPConnection::tick(const size_t ms_since_last_tick) {
+    DUMMY_CODE(ms_since_last_tick);
+}
 
-void TCPConnection::end_input_stream() {}
+void TCPConnection::end_input_stream() {
+    _sender.stream_in().end_input();
+}
 
-void TCPConnection::connect() {}
+void TCPConnection::connect() {
+
+}
 
 TCPConnection::~TCPConnection() {
     try {
--- libsponge/tcp_connection.hh
+++ libsponge/tcp_connection.hh
@@ -21,6 +21,8 @@ class TCPConnection {
     //! in case the remote TCPConnection doesn't know we've received its whole stream?
     bool _linger_after_streams_finish{true};
 
+    size_t timeSinceLastSegmentReceived;
+
   public:
     //! \name "Input" interface for the writer
     //!@{
